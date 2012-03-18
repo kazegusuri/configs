0755c51ebb72c930f0730afde2fe98aed7813ce5
sabottenda <sabottenda@gmail.como>
2012-03-19 00:42:15 +0900

    use init-loader.el and update anything and autocomplete

--- emacs/dot.css.el
+++
@@ -1,8 +0,0 @@
-;; css-mode
-(autoload 'css-mode "css-mode")
-(setq auto-mode-alist
-      (cons '("\\.css\\'" . css-mode) auto-mode-alist))
-(setq cssm-indent-function #'cssm-c-style-indenter)
-(add-hook 'css-mode-hook
-          '(lambda ()
-             (define-key cssm-mode-map (read-kbd-macro "C-o") 'cssm-complete-property)))
--- emacs/dot.emacs.el
+++ emacs/dot.emacs.el
@@ -1,14 +1,12 @@
 
 ;; lisp load path recursively
-(let ((default-directory "~/local/common/emacs/site-lisp"))
+(let ((default-directory "~/local/common/emacs/elisp"))
   (setq load-path (cons default-directory load-path))
 ;;  (setq load-path (cons "~/.emacs.d/site-lisp" load-path))
   (normal-top-level-add-subdirs-to-load-path))
 
-(load "~/local/common/emacs/dot.style.el");
-(load "~/local/common/emacs/dot.flymake.el");
-(load "~/local/common/emacs/dot.common.el");  // should load after dot.style.el
-;;(load "~/local/common/emacs/dot.yatex.el");
-(load "~/local/common/emacs/dot.anything.el");
-(load "~/local/common/emacs/dot.elscreen.el");
-(load "~/local/common/emacs/dot.autocomplete.el");
+;; init-loader
+;; (auto-install-from-gist "https://gist.github.com/1021706")
+(require 'init-loader)
+(setq init-loader-show-log-after-init nil)
+(init-loader-load "~/local/common/emacs/load")
