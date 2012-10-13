866f4f78eac62627aa205ec157a3b67fbfe43449
Masahiro Sano <sabottenda@gmail.como>
2012-10-14 00:44:40 +0900

    add eldoc settings

---
+++ emacs/load/50-dot.eldoc.el
@@ -0,0 +1,10 @@
+;;; eldoc
+;;(install-elisp "http://www.emacswiki.org/cgi-bin/wiki/download/eldoc-extension.el")
+(require 'eldoc-extention)
+(setq eldoc-idle-delay 0.5)
+(setq eldoc-minor-mode-string "")
+(setq eldoc-echo-area-use-multiline-p t)
+
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
