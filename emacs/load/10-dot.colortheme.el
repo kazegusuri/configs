142db86c01e72ffe446ed76515f551abd3359414
Masahiro Sano <sabottenda@gmail.com>
2013-02-22 18:46:32 +0900

    modify color-theme setting

--- emacs/load/10-dot.colortheme.el
+++ emacs/load/10-dot.colortheme.el
@@ -1,9 +1,8 @@
 ;; coloring
 (global-font-lock-mode t)
 
+;; color-theme directory
+(setq custom-theme-directory "~/local/common/emacs/theme")
+
 ;;; color-theme
-(custom-set-variables 
- '(custom-enabled-themes (quote (zenburn)))
- '(custom-safe-themes (quote ("d6a00ef5e53adf9b6fe417d2b4404895f26210c52bb8716971be106550cea257" default)))
- '(custom-theme-directory "~/local/common/emacs/theme")
-)
+(load-theme 'zenburn t)
