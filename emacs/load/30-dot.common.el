3d0052bb2af1029d86d228ac8558081ddb17f4f4
Masahiro Sano <sabottenda@gmail.com>
2014-06-30 07:26:00 +0900

    add hightligh-symbol and whitespace

--- emacs/load/30-dot.common.el
+++ emacs/load/30-dot.common.el
@@ -213,3 +213,34 @@
 ;; golden-ratio
 (require 'golden-ratio)
 (golden-ratio-mode 1)
+
+;; auto-highlight-symbol
+(require 'auto-highlight-symbol)
+(global-auto-highlight-symbol-mode t)
+(ahs-set-idle-interval 0.3)
+
+;; whitespace
+(require 'whitespace)
+(setq whitespace-style '(face spaces trailing space-mark tab-mark))
+(setq whitespace-display-mappings
+      '(
+        ;; (space-mark ?\ [?\u00B7] [?.]) ; space - centered dot
+        (space-mark ?\xA0 [?\u00A4] [?_]) ; hard space - currency
+        (space-mark ?\x8A0 [?\x8A4] [?_]) ; hard space - currency
+        (space-mark ?\x920 [?\x924] [?_]) ; hard space - currency
+        (space-mark ?\xE20 [?\xE24] [?_]) ; hard space - currency
+        (space-mark ?\xF20 [?\xF24] [?_]) ; hard space - currency
+        (space-mark ?\u3000 [?\u25a1] [?_ ?_]) ; full-width-space - square
+        ;; (newline-mark ?\n [?$ ?\n]) ; eol - dollar sign
+        (tab-mark ?\t [?\u00BB ?\t] [?\\ ?\t]) ; tab - left quote mark
+        ))
+
+;; whitespace-spaceの定義を全角スペースにし、色をつけて目立たせる
+(setq whitespace-space-regexp "\\(\u3000+\\)")
+(set-face-foreground 'whitespace-space "yellow")
+(set-face-background 'whitespace-space 'nil)
+;; whitespace-trailingを色つきアンダーラインで目立たせる
+(set-face-underline 'whitespace-trailing t)
+(set-face-foreground 'whitespace-trailing "cyan")
+(set-face-background 'whitespace-trailing 'nil)
+(global-whitespace-mode 1)
