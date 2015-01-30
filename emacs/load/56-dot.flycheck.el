c3a5c8518ba98763609f5f6c3350cdb3b42ff3ff
Masahiro Sano <sabottenda@gmail.com>
2015-01-30 17:23:48 +0900

    modify flycheck.el

--- emacs/load/56-dot.flycheck.el
+++ emacs/load/56-dot.flycheck.el
@@ -3,6 +3,8 @@
   '(lambda ()
      (custom-set-variables
       '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)
+      '(flycheck-highlighting-mode 'lines)
+
       )
      (add-hook 'flycheck-mode-hook
                'flycheck-color-mode-line-mode
@@ -11,6 +13,9 @@
 
 (setq flycheck-display-errors-delay 0.5)
 (setq flycheck-pos-tip-timeout 3)
+
+(global-set-key "\M-p" 'flycheck-previous-error)
+(global-set-key "\M-n" 'flycheck-next-error)
 
 (defun my-flycheck-display-error-at-point ()
   (interactive)
