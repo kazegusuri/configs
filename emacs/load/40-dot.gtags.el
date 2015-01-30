28603499dfe89a85f48d53ce8cccb48412e7c838
Masahiro Sano <sabottenda@gmail.com>
2015-01-30 17:24:16 +0900

    always use gtags-mode

--- emacs/load/40-dot.gtags.el
+++ emacs/load/40-dot.gtags.el
@@ -3,6 +3,7 @@
 (autoload 'gtags-mode "gtags" "" t)
 (add-hook 'gtags-mode-hook
       '(lambda ()
+         (setenv "GTAGSLABEL" "pygments")
          (local-set-key "\M-t" 'gtags-find-tag)
          (local-set-key "\M-r" 'gtags-find-rtag)
          (local-set-key "\M-s" 'gtags-find-symbol)
@@ -10,13 +11,24 @@
          ))
 (add-hook 'cscope-list-entry-hook 'hl-line-mode)
 (add-hook 'gtags-select-mode-hook 'hl-line-mode)
-(defun my-update-gtags () 
-  (interactive) 
+
+(defun my-gtags-root-dir ()
+  "Returns GTAGS root directory or nil if doesn't exist."
+  (with-temp-buffer
+    (if (zerop (call-process "global" nil t nil "-pr"))
+        (buffer-substring (point-min) (1- (point-max)))
+      nil)))
+
+(defun my-gtags-update ()
+  "Make GTAGS incremental update"
+  (interactive)
   (when (executable-find "global")
-    (call-process "global" nil nil nil "-uv")))
-(add-hook
- 'c-mode-common-hook
- '(lambda()
-    (add-hook 'after-save-hook 'my-update-gtags)))
+    (call-process "global" nil nil nil "-u")))
+
+(defun my-gtags-update-hook ()
+  (when (my-gtags-root-dir)
+    (my-gtags-update)))
+
+(add-hook 'after-save-hook #'my-gtags-update-hook)
 
 (setq anything-gtags-hijack-gtags-select-mode nil)
