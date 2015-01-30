1562b7c132f22595443dc80c83773c9599d61510
Masahiro Sano <sabottenda@gmail.com>
2015-01-30 17:23:25 +0900

    add go.el

---
+++ emacs/load/65-dot.go.el
@@ -0,0 +1,23 @@
+(require 'go-mode)
+
+(add-hook 'go-mode-hook
+          '(lambda ()
+             ;; Need gocode
+             ;; $ go get -u github.com/nsf/gocode
+             (require 'go-autocomplete)
+
+             (local-set-key (kbd "\C-c d") 'godoc)
+             (local-set-key (kbd "\C-c i") 'go-goto-imports)
+             (local-set-key (kbd "\C-c a") 'go-import-add)
+             (local-set-key (kbd "\C-c r") 'go-remove-unused-imports)
+             ;; Need godef
+             ;; $ go get -u code.google.com/p/rog-go/exp/cmd/godef
+             (local-set-key (kbd "M-.") 'godef-jump)
+
+             ;; run gofmt before save
+             (add-hook 'before-save-hook 'gofmt-before-save)
+
+             ))
+
+(require 'flycheck)
+(add-hook 'go-mode-hook 'flycheck-mode)
